f5355cdfabf6319ca51bff9eb053465f29091209
Horrowind <Horrowind@users.noreply.github.com>
2014-10-10 14:57:21 +0200

    Update main_window.hpp
    Remove standard library.

--- app/main_window.hpp
+++ app/main_window.hpp
@@ -1,8 +1,6 @@
 #ifndef MOCKUP_MAIN_WINDOW_HPP
 #define MOCKUP_MAIN_WINDOW_HPP
 
-#include <vector>
-
 #include <QMainWindow>
 #include <QGraphicsView>
 
@@ -31,10 +29,6 @@ namespace Mockup {
         
         void draw_level(int level);
 
-        // std::vector<QPixmap> load_map16(const char* path, int level, std::vector<QImage> map8  ,std::vector<uint32_t> palette );
-        // std::vector<QImage> load_map8(const char* path, int level);
-        // std::vector<uint32_t> load_palette(const char* path, int level);
-        
         QGraphicsView* view;
         QGraphicsScene scene;
         
