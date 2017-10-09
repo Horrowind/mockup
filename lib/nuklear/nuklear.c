4b535d3d618c2a798788814cf650a2d16d2277ba
horrowind <>
2017-10-09 03:19:47 +0200

    Moved nuklear from app/ to lib/

---
+++ lib/nuklear/nuklear.c
@@ -0,0 +1,13 @@
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_GLFW_GL3_IMPLEMENTATION
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_VSNPRINTF(s,n,f,a) nk_vsnprintf(s,n,f,a)
+#include "nuklear.h"
+#include "nuklear_glfw_gl3.h"
+
