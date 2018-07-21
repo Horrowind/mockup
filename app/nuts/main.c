1af41b2367c228d3620be86bd8f44d5937d3d4c5
Horrowind <you@example.com>
2018-07-21 12:53:36 +0200

    First iteration at a working assembler.

--- app/nuts/main.c
+++ app/nuts/main.c
@@ -1,30 +1,31 @@
 #include <time.h>
 #include "nuts/nuts.h"
 
-struct timespec timespec_add(struct timespec start, struct timespec end) {
-	struct timespec tmp;
-	if ((end.tv_nsec+start.tv_nsec) >= 1000000000) {
-		tmp.tv_sec  = end.tv_sec  + start.tv_sec  + 1;
-		tmp.tv_nsec = end.tv_nsec + start.tv_nsec - 1000000000;
-	} else {
-		tmp.tv_sec  = end.tv_sec  + start.tv_sec;
-		tmp.tv_nsec = end.tv_nsec + start.tv_nsec;
-	}
-	return tmp;
-}
+
+/* struct timespec timespec_add(struct timespec start, struct timespec end) { */
+/* 	struct timespec tmp; */
+/* 	if ((end.tv_nsec+start.tv_nsec) >= 1000000000) { */
+/* 		tmp.tv_sec  = end.tv_sec  + start.tv_sec  + 1; */
+/* 		tmp.tv_nsec = end.tv_nsec + start.tv_nsec - 1000000000; */
+/* 	} else { */
+/* 		tmp.tv_sec  = end.tv_sec  + start.tv_sec; */
+/* 		tmp.tv_nsec = end.tv_nsec + start.tv_nsec; */
+/* 	} */
+/* 	return tmp; */
+/* } */
 
 
-struct timespec timespec_sub(struct timespec start, struct timespec end) {
-	struct timespec tmp;
-	if ((end.tv_nsec-start.tv_nsec)<0) {
-		tmp.tv_sec = end.tv_sec-start.tv_sec-1;
-		tmp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec;
-	} else {
-		tmp.tv_sec = end.tv_sec-start.tv_sec;
-		tmp.tv_nsec = end.tv_nsec-start.tv_nsec;
-	}
-	return tmp;
-}
+/* struct timespec timespec_sub(struct timespec start, struct timespec end) { */
+/* 	struct timespec tmp; */
+/* 	if ((end.tv_nsec-start.tv_nsec)<0) { */
+/* 		tmp.tv_sec = end.tv_sec-start.tv_sec-1; */
+/* 		tmp.tv_nsec = 1000000000+end.tv_nsec-start.tv_nsec; */
+/* 	} else { */
+/* 		tmp.tv_sec = end.tv_sec-start.tv_sec; */
+/* 		tmp.tv_nsec = end.tv_nsec-start.tv_nsec; */
+/* 	} */
+/* 	return tmp; */
+/* } */
 
 int main(int argc, char** argv) {
     if(argc != 2 && argc != 3) {
@@ -33,15 +34,38 @@ int main(int argc, char** argv) {
     }
     Arena arena = arena_create(MB(512));
 
-    /* String cartrige_folder_path_name = (argc > 2) ? string_from_c_string(argv[2]) : L("SuperMarioWorld.sfc"); */
-    /* Path cartrige_folder_path; */
-    /* path_init(&cartrige_folder_path, cartrige_folder_path_name); */
-    /* VFS cartrige_folder_vfs; */
-    /* vfs_init(&cartrige_folder_vfs, 4); */
-    /* vfs_add_dir(&cartrige_folder_vfs, &cartrige_folder_path, &arena); */
+    String cartridge_folder_path_name = (argc > 2)
+        ? string_from_c_string(argv[2])
+        : L("SuperMarioWorld.sfc");
+    Path cartridge_folder_path;
+    path_init(&cartridge_folder_path, cartridge_folder_path_name);
+    Path manifest_path;
+    path_init_from_c(&manifest_path, &cartridge_folder_path, "manifest.bml");
+    Buffer manifest_buffer = path_read_file(&manifest_path, &arena);
 
-    /* Wdc65816Rom rom; */
-    /* wdc65816_rom_init(&rom, &cartrige_folder_vfs, &arena); */
+    Wdc65816MapperBuilder rom_builder = { };
+    
+    char name[256];
+    Buffer name_buffer = buffer(name, 256);
+    Buffer rom_buffer;
+    for(Wdc65816RomLoader loader = wdc65816_rom_loader_begin(&rom_builder,
+                                                             string_from_buffer(manifest_buffer));
+        wdc65816_rom_loader_end(&loader);
+        wdc65816_rom_loader_next(&loader)) {
+        Wdc65816MemoryBufferRequest request = wdc65816_rom_loader_get_buffer_request(&loader,
+                                                                                     name_buffer);
+        Buffer file_buffer = buffer(arena_alloc_array(&arena, request.size, u8), request.size);
+        if(string_equal(request.name, L("program.rom")) &&
+           request.type == WDC65816_MEMORY_ROM) {
+            rom_buffer = file_buffer;
+        }
+        wdc65816_rom_loader_set_buffer(&loader, file_buffer);
+    }
+
+    uint mapper_buffer_size = wdc65816_mapper_get_buffer_size();
+    u8* work_buffer = arena_alloc_array(&arena, mapper_buffer_size, u8);
+    Wdc65816Mapper rom;
+    wdc65816_mapper_init(&rom, &rom_builder, (u8**)work_buffer);
 
     nuts_global_init();
 
@@ -53,22 +77,19 @@ int main(int argc, char** argv) {
     error_list_init(&error_list, arena_subarena(&arena, MB(10)));
 
 
-    StatementList stmt_list;
+    AST ast;
+    ast_init(&ast, &arena);
     String file_name = string_from_c_string(argv[1]); 
-    struct timespec lex_start;
-    struct timespec lex_end;
-    struct timespec lex_time = { 0 };
-    struct timespec parse_start;
-    struct timespec parse_end;
-    struct timespec parse_time = { 0 };
-    struct timespec all_start;
-    struct timespec all_end;
-    struct timespec all_time;
+    /* struct timespec lex_start,      lex_end,      lex_time = { 0 }; */
+    /* struct timespec parse_start,    parse_end,    parse_time = { 0 }; */
+    /* struct timespec assemble_start, assemble_end, assemble_time = { 0 }; */
+    /* struct timespec all_start,      all_end,      all_time; */
     
 
-    clock_gettime(CLOCK_REALTIME, &all_start);
+    /* clock_gettime(CLOCK_REALTIME, &all_start); */
     Parser parser;
-    parser_init(&parser, &arena, &sentinel, &error_list, &stmt_list);
+    parser_init(&parser, &arena, &sentinel, &error_list, &ast);
+    int error_num = 0;
     while(1) {
         Path file;
         path_init(&file, file_name);
@@ -79,31 +100,38 @@ int main(int argc, char** argv) {
             .buffer = file_buffer,
             .name   = file_name
         };
-        clock_gettime(CLOCK_REALTIME, &lex_start);
-        lex(file_text, &token_list, &arena, &error_list);
-        clock_gettime(CLOCK_REALTIME, &lex_end);
-        lex_time = timespec_add(lex_time, timespec_sub(lex_start, lex_end));
-        //token_list_print(token_list);
-        clock_gettime(CLOCK_REALTIME, &parse_start);
-        Result result = parse(&parser, token_list);
-        clock_gettime(CLOCK_REALTIME, &parse_end);
-        parse_time = timespec_add(parse_time, timespec_sub(parse_start, parse_end));
+        /* clock_gettime(CLOCK_REALTIME, &lex_start); */
+        Result result = lex(file_text, &token_list, &arena, &error_list, &ast.identifier_map);
+        if(result == RESULT_ERROR) {
+            for(;error_num < error_list.length; error_num++) {
+                describe_error(error_list.errors[error_num]);
+            }
+        }
+        /* clock_gettime(CLOCK_REALTIME, &lex_end); */
+        /* lex_time = timespec_add(lex_time, timespec_sub(lex_start, lex_end)); */
+        /* clock_gettime(CLOCK_REALTIME, &parse_start); */
+        /* result = parse(&parser, token_list); */
+        /* clock_gettime(CLOCK_REALTIME, &parse_end); */
+        /* parse_time = timespec_add(parse_time, timespec_sub(parse_start, parse_end)); */
 
         if(result == RESULT_NEED_TOKEN_STREAM) {
             file_name = parser.needed_token_stream_file_name;
             continue;
         } else if(result == RESULT_OK) {
             break;
+        } else if(result == RESULT_ERROR) {
+            for(;error_num < error_list.length; error_num++) {
+                describe_error(error_list.errors[error_num]);
+            }
+            return 1;
+        } else {
+            invalid_code_path;
         }
     }
 
-    for(int i = 0; i < error_list.length; i++) {
-        describe_error(error_list.errors[i]);
-    }
-    
-#if 0
+    /* clock_gettime(CLOCK_REALTIME, &assemble_start); */
     Assembler assembler;
-    assembler_init(&assembler, &error_list, parser.statement_list, &arena);
+    assembler_init(&assembler, &error_list, &ast, &rom);
     Result result = RESULT_ERROR;
     while(result != RESULT_OK) {
         result = assemble(&assembler);
@@ -112,16 +140,29 @@ int main(int argc, char** argv) {
             Path file;
             path_init(&file, file_name);
             Buffer file_buffer = path_read_file(&file, &arena);
-            assembler_give_file(&assembler, file_buffer);
+            assembler_give_buffer(&assembler, file_buffer);
+        } else if(result == RESULT_ERROR) {
+            break;
         }
     }
-#endif
-    parser_deinit(&parser);
-    clock_gettime(CLOCK_REALTIME, &all_end);
-    all_time = timespec_sub(all_start, all_end);
-    printf("Lex:   %li.%06lims\n", lex_time.tv_sec * 1000 + lex_time.tv_nsec / 1000000, lex_time.tv_nsec % 1000000);
-    printf("Parse: %li.%06lims\n", parse_time.tv_sec * 1000 + parse_time.tv_nsec / 1000000, parse_time.tv_nsec % 1000000);
-    printf("All:   %li.%06lims\n", all_time.tv_sec * 1000 + all_time.tv_nsec / 1000000, all_time.tv_nsec % 1000000);
+    /* clock_gettime(CLOCK_REALTIME, &assemble_end); */
+    /* assemble_time = timespec_add(assemble_time, timespec_sub(assemble_start, assemble_end)); */
 
+    for(int i = 0; i < error_list.length; i++) {
+        describe_error(error_list.errors[i]);
+    }
+
+    /* parser_deinit(&parser); */
+    /* clock_gettime(CLOCK_REALTIME, &all_end); */
+    /* all_time = timespec_sub(all_start, all_end); */
+    /* printf("Lex:      %li.%06lims\n",      lex_time.tv_sec * 1000 +      lex_time.tv_nsec / 1000000,      lex_time.tv_nsec % 1000000); */
+    /* printf("Parse:    %li.%06lims\n",    parse_time.tv_sec * 1000 +    parse_time.tv_nsec / 1000000,    parse_time.tv_nsec % 1000000); */
+    /* printf("Assemble: %li.%06lims\n", assemble_time.tv_sec * 1000 + assemble_time.tv_nsec / 1000000, assemble_time.tv_nsec % 1000000); */
+    /* printf("All:      %li.%06lims\n",      all_time.tv_sec * 1000 +      all_time.tv_nsec / 1000000,      all_time.tv_nsec % 1000000); */
+
+    Path rom_path;
+    path_create_from(&rom_path, &cartridge_folder_path, L("program.rom"));
+    path_write_file(&rom_path, rom_buffer);
+    
     return 0;
 }
