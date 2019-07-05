f3c19763699f8ad6932b5c4893d61d71d485113f
Horrowind <you@example.com>
2019-07-05 23:12:01 +0200

    Added config.mk

---
+++ config.mk
@@ -0,0 +1,8 @@
+COMPILER_LINUX   := GCC
+CC_LINUX         := gcc
+LD_LINUX         := gcc
+COMPILER_WINDOWS := GCC
+CC_WINDOWS       := i686-w64-mingw32-gcc
+LD_WINDOWS       := i686-w64-mingw32-gcc
+CC_WASM          := clang-8
+LD_WASM          := wasm-ld-8
